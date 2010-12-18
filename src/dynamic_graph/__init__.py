d84a8c8688a1de1dae42f48e6cb5a3c361cf03ee
florent <florent@laas.fr>
2010-12-18 17:32:49 +0100

    Set flags of dynamic loader so that symbols are global.
        * src/dynamic_graph/__init__.py.

--- src/dynamic_graph/__init__.py
+++ src/dynamic_graph/__init__.py
@@ -3,7 +3,15 @@ Copyright (c) 2010 CNRS
 Author: Florent Lamiraux
 """
 
+import sys, dl
+flags = sys.getdlopenflags()
+# Import C++ symbols in a global scope
+# This is necessary for signal compiled in different modules to be compatible
+sys.setdlopenflags(dl.RTLD_NOW|dl.RTLD_GLOBAL)
 from wrap import *
+# Recover previous flags
+sys.setdlopenflags(flags)
+
 import entity, signal_base
 import re
 
