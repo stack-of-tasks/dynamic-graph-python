45f00cff17c14313efdeee9ef07b0b8d5858d913
Olivier Stasse <olivier.stasse@gmail.com>
2013-07-09 10:00:26 +0200

    Add mapping to getClassName.

--- src/signal-base-py.cc
+++ src/signal-base-py.cc
@@ -242,6 +242,26 @@ namespace dynamicgraph {
 	return Py_BuildValue("s", name.c_str());
       }
 
+      PyObject* getClassName(PyObject* /*self*/, PyObject* args)
+      {
+	void* pointer = NULL;
+	PyObject* object = NULL;
+	if (!PyArg_ParseTuple(args,"O", &object))
+	  return NULL;
+	if (!PyCObject_Check(object))
+	  return NULL;
+
+	pointer = PyCObject_AsVoidPtr(object);
+	SignalBase<int>* signal = (SignalBase<int>*)pointer;
+
+	std::string name;
+	try {
+          signal->getClassName(name);
+	} CATCH_ALL_EXCEPTIONS ();
+
+	return Py_BuildValue("s", name.c_str());
+      }
+
       PyObject* setValue(PyObject* /*self*/, PyObject* args)
       {
 	void * pointer = NULL;
