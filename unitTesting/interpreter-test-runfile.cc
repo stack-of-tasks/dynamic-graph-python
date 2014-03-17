4c1927049e2f3c4befb38c33496ccca1e18b5bbf
Francois Keith <keith@lirmm.fr>
2014-03-18 00:08:30 +0100

    Add a working python file to test the interpreter.

--- unitTesting/interpreter-test-runfile.cc
+++ unitTesting/interpreter-test-runfile.cc
@@ -1,5 +1,9 @@
 // The purpose of this unit test is to check the interpreter::runPythonFile method
+#include <cstring>
+#include <iostream>
+
 #include "dynamic-graph/python/interpreter.hh"
+
 
 int main(int argc, char ** argv)
 {
@@ -10,11 +14,34 @@ int main(int argc, char ** argv)
   if (argc > 1)
     numTest = atoi(argv[1]);
 
+  std::string empty_err = "";
   dynamicgraph::python::Interpreter interp;
 
   for (int i=0; i<numTest; ++i)
   {
-    interp.runPythonFile("test_python_error.py");
+    interp.runPythonFile("test_python_ok.py", empty_err);
+    if (empty_err != "")
+    {
+      std::cerr << "At iteration " << i << ", the error was not empty:" << std::endl;
+      std::cerr << " err " << empty_err << std::endl;
+      return -1;
+    }
+  }
+
+  // check that the error remains the same, despite of the number of executions
+  std::string old_err;
+  interp.runPythonFile("test_python_error.py", old_err);
+  std::string new_err = old_err;
+  for (int i=0; i<numTest; ++i)
+  {
+    interp.runPythonFile("test_python_error.py", new_err);
+    if (old_err != new_err)
+    {
+      std::cerr << "At iteration " << i << ", the error changed:" << std::endl;
+      std::cerr << " old " << old_err << std::endl;
+      std::cerr << " new " << new_err << std::endl;
+      return -1;
+    }
   }
 
   return 0;
