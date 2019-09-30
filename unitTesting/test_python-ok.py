a6b5192497afd534aab16b2f138e4b2a33c752bc
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-09-30 17:00:10 +0200

    remove interpreter

--- unitTesting/test_python-ok.py
+++
@@ -1,9 +0,0 @@
-import os
-import re
-
-pkgConfigPath = os.environ.get("PKG_CONFIG_PATH")
-if pkgConfigPath is None:
-    pkgConfigPath = ''
-pathList = re.split(':', pkgConfigPath)
-
-print(pathList)
