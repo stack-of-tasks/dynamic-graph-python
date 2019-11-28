6e43bd1f9deaa100416f8d13e870d4f225e2a4b9
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-11-28 15:16:36 +0100

    [Tests] checks that connecting a signal to an entity raises an exception
    For now, this test doesn't pass, as an exception is not properly fired,
    but we get a segfault instead.

--- unitTesting/test_bindings.py
+++ unitTesting/test_bindings.py
@@ -1,13 +1,23 @@
 import unittest
 
-import dynamic_graph
+import dynamic_graph as dg
+from custom_entity import CustomEntity
 
 
 class BindingsTests(unittest.TestCase):
     def test_bindings(self):
         with self.assertRaises(Exception) as error:
-            dynamic_graph.error_out()
+            dg.error_out()
             self.assertEqual(str(error), "something bad happend")
+
+    def test_type_check(self):
+        first = CustomEntity('first_entity')
+        second = CustomEntity('second_entity')
+        # Check that we can connect first.out to second.in
+        dg.plug(first.signal('out_double'), second.signal('in_double'))
+        # Check that we can't connect first.out to second
+        with self.assertRaises(Exception):
+            dg.plug(first.signal('out_double'), second)
 
 
 if __name__ == '__main__':
