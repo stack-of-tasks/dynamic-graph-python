aaa8f2f00c3ae1f8e46419fc49019731bb793ecd
Joseph Mirabel <jmirabel@laas.fr>
2020-06-11 12:47:27 +0200

    Switch to Boost Python

--- src/dynamic_graph/debug-py.cc
+++ src/dynamic_graph/debug-py.cc
@@ -11,7 +11,6 @@
 #include <dynamic-graph/pool.h>
 #include <dynamic-graph/entity.h>
 #include <vector>
-#include "dynamic-graph/python/exception.hh"
 
 #include <boost/shared_ptr.hpp>
 #include "dynamic-graph/python/dynamic-graph-py.hh"
@@ -21,107 +20,45 @@ typedef boost::shared_ptr<std::ofstream> ofstreamShrPtr;
 namespace dynamicgraph {
 namespace python {
 
-#if PY_MAJOR_VERSION == 2
-  extern PyObject* dgpyError;
-# endif
-
 namespace debug {
 
 std::map<std::string, ofstreamShrPtr> mapOfFiles_;
 
-PyObject* addLoggerFileOutputStream(
-#if PY_MAJOR_VERSION >= 3
-    PyObject* m, PyObject* args
-#else
-    PyObject*, PyObject* args
-#endif
-    ) {
-  char* filename;
-  if (!PyArg_ParseTuple(args, "s", &filename)) return NULL;
-  std::string sfilename(filename);
-  try {
-    std::ofstream* aofs = new std::ofstream;
-    ofstreamShrPtr ofs_shrptr = boost::shared_ptr<std::ofstream>(aofs);
-    aofs->open(filename, std::ofstream::out);
-    dynamicgraph::RealTimeLogger::instance();
-    dgADD_OSTREAM_TO_RTLOG(*aofs);
-    dgRTLOG() << "Added " << filename << " as an output stream \n";
-    mapOfFiles_[sfilename] = ofs_shrptr;
-  }
-  CATCH_ALL_EXCEPTIONS(m);
-  return Py_BuildValue("");
+void addLoggerFileOutputStream(const char* filename)
+{
+  std::ofstream* aofs = new std::ofstream;
+  ofstreamShrPtr ofs_shrptr = boost::shared_ptr<std::ofstream>(aofs);
+  aofs->open(filename, std::ofstream::out);
+  dynamicgraph::RealTimeLogger::instance();
+  dgADD_OSTREAM_TO_RTLOG(*aofs);
+  dgRTLOG() << "Added " << filename << " as an output stream \n";
+  mapOfFiles_[filename] = ofs_shrptr;
 }
 
-PyObject* closeLoggerFileOutputStream(
-#if PY_MAJOR_VERSION >= 3
-    PyObject* m, PyObject*
-#else
-    PyObject*, PyObject*
-#endif
-    ) {
-  try {
-    for (std::map<std::string, ofstreamShrPtr>::iterator it = mapOfFiles_.begin(); it != mapOfFiles_.end(); ++it) {
-      it->second->close();
-    }
-  }
-  CATCH_ALL_EXCEPTIONS(m);
-  return Py_BuildValue("");
+void closeLoggerFileOutputStream()
+{
+  for (const auto& el : mapOfFiles_)
+    el.second->close();
 }
 
-PyObject* addLoggerCoutOutputStream(
-#if PY_MAJOR_VERSION >= 3
-    PyObject* m, PyObject*
-#else
-    PyObject*, PyObject*
-#endif
-    ) {
-  try {
-    dgADD_OSTREAM_TO_RTLOG(std::cout);
-  }
-  CATCH_ALL_EXCEPTIONS(m);
-  return Py_BuildValue("");
+void addLoggerCoutOutputStream()
+{
+  dgADD_OSTREAM_TO_RTLOG(std::cout);
 }
 
-PyObject* realTimeLoggerDestroy(
-#if PY_MAJOR_VERSION >= 3
-    PyObject* m, PyObject*
-#else
-    PyObject*, PyObject*
-#endif
-    ) {
-  try {
-    RealTimeLogger::destroy();
-  }
-  CATCH_ALL_EXCEPTIONS(m);
-  return Py_BuildValue("");
+void realTimeLoggerDestroy()
+{
+  RealTimeLogger::destroy();
 }
 
-PyObject* realTimeLoggerSpinOnce(
-#if PY_MAJOR_VERSION >= 3
-    PyObject* m, PyObject*
-#else
-    PyObject*, PyObject*
-#endif
-    ) {
-  try {
-    RealTimeLogger::instance().spinOnce();
-  }
-  CATCH_ALL_EXCEPTIONS(m);
-  return Py_BuildValue("");
+void realTimeLoggerSpinOnce()
+{
+  RealTimeLogger::instance().spinOnce();
 }
 
-PyObject* realTimeLoggerInstance(
-#if PY_MAJOR_VERSION >= 3
-    PyObject* m, PyObject*
-#else
-    PyObject*, PyObject*
-#endif
-    ) {
-  try {
-    RealTimeLogger::instance();
-  }
-  CATCH_ALL_EXCEPTIONS(m);
-  return Py_BuildValue("");
+void realTimeLoggerInstance()
+{
+  RealTimeLogger::instance();
 }
 
 }  // namespace debug
--- src/dynamic_graph/signal_base.py
+++ src/dynamic_graph/signal_base.py
@@ -1,18 +1,14 @@
-"""
-  Copyright (C) 2010 CNRS
-
-  Author: Florent Lamiraux
-"""
+# Copyright (C) 2020 CNRS
+#
+# Author: Joseph Mirabel
 
 from __future__ import print_function
 
+from .wrap import SignalBase, create_signal_wrapper as SignalWrapper
+
+# I kept what follows for backward compatibility but I think it should be
+# removed
 import re
-
-from .wrap import (create_signal_wrapper, signal_base_display, signal_base_display_dependencies,
-                   signal_base_get_class_name, signal_base_get_name, signal_base_get_time, signal_base_get_value,
-                   signal_base_getPlugged, signal_base_isPlugged, signal_base_recompute, signal_base_set_time,
-                   signal_base_set_value, signal_base_unplug)
-
 
 def stringToTuple(vector):
     """
@@ -153,136 +149,3 @@ def stringToObject(string):
         return float(string)
     except Exception:
         return string
-
-
-class SignalBase(object):
-    """
-    This class binds dynamicgraph::SignalBase<int> C++ class
-    """
-
-    obj = None
-
-    def __init__(self, name="", obj=None):
-        """
-        Constructor: if not called by a child class, create and store a pointer
-        to a C++ SignalBase<int> object.
-        """
-        if obj:
-            self.obj = obj
-        else:
-            raise RuntimeError("A pointer is required to create SignalBase object.")
-
-        if obj is None:
-            self.className = self.getClassName()
-            self.name = self.getName()
-
-    @property
-    def time(self):
-        """
-        Get time of signal
-        """
-        return signal_base_get_time(self.obj)
-
-    @time.setter
-    def time(self, val):
-        """
-        Set Time of signal
-
-          Input:
-            - an integer
-        """
-        return signal_base_set_time(self.obj, val)
-
-    @property
-    def value(self):
-        """
-        Setter and getter for the value of a signal
-
-        Binds C++ SignalBase<int>::get() and set() methods. Values are passed
-        through string streams.
-        A string is interpreted as respectively:
-        * a matrix (tuple of tuple) if string fits '[n,m]((x_11,x_12,...,x_1m),...,(x_n1,x_n2,...,x_nm))' format where
-          n and m are integers, x_ij are floating point numbers,
-        * a tuple if string fits '[n](x_1, x_2, ..., x_n)' format,
-        * an integer,
-        * a floating point number.
-
-        If string fits none of the above formats, no conversion is performed.
-
-        For instance, is s binds a signal of type vector,
-        >>> s.value = (2.5, .1, 1e2)
-        will call SignalBase<int>::set("[3](2.5,0.1,100.0)") and
-        >>> s.value
-        (2.5, 0.1, 100.0)
-        """
-        string = signal_base_get_value(self.obj)
-        return stringToObject(string)
-
-    @value.setter
-    def value(self, val):
-        """
-        Set the signal as a constant signal with given value.
-        If the signal is plugged, it will be unplugged
-        """
-        string = objectToString(val)
-        return signal_base_set_value(self.obj, string)
-
-    def getName(self):
-        """
-        Get name of signal
-        """
-        return signal_base_get_name(self.obj)
-
-    @property
-    def name(self):
-        """
-        Get name of signal
-        """
-        return signal_base_get_name(self.obj)
-
-    def getClassName(self):
-        """
-        Get class name of signal
-        """
-        return signal_base_get_class_name(self.obj)
-
-    def recompute(self, time):
-        """
-        Force signal to recompute the value at given time.
-        """
-        return signal_base_recompute(self.obj, time)
-
-    def unplug(self):
-        """
-        Unplug a PTR signal.
-        """
-        return signal_base_unplug(self.obj)
-
-    def isPlugged(self):
-        """
-        Return whether a signal is plugged.
-        """
-        return signal_base_isPlugged(self.obj)
-
-    def getPlugged(self):
-        """
-        Return the plugged signal.
-        """
-        return SignalBase(obj=signal_base_getPlugged(self.obj))
-
-    def __str__(self):
-        """
-        Print signal in a string
-        """
-        return signal_base_display(self.obj)
-
-    def displayDependencies(self, iter):
-        """
-        Print signal dependencies in a string
-        """
-        return (signal_base_display_dependencies(self.obj, iter))
-
-
-class SignalWrapper(SignalBase):
-    def __init__(self, name, type, func):
-        super(SignalWrapper, self).__init__(name, create_signal_wrapper(name, type, func))
